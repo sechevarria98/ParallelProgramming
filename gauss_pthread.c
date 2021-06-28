ffb4d6a75ffb516bb0a0d03f76ded1c01c4e4777
sechevarria98 <samechevarria2@gmail.com>
2021-06-28 16:27:42 -0500

    pushing the pthread version of gaussian elimination

--- gauss_pthread.c
+++ gauss_pthread.c
@@ -6,6 +6,7 @@
 #include <sys/times.h>
 #include <sys/time.h>
 #include <time.h>
+#include <pthread.h>
 
 /* Program Parameters */
 #define MAXN 2000  /* Max value of N */
@@ -14,16 +15,13 @@ int numThreads; /* Number of threads */
 
 /* Matrices and vectors */
 volatile float A[MAXN][MAXN], B[MAXN], X[MAXN];
-/* A * X = B, solve for X */
 
-/* junk */
-// #define randm() 4|2[uid]&3
+struct thread_args {
+    int id;
+    int norm;
+} thread_args;
 
-/* Prototype */
-void gauss();  /* The function you will provide.
-		* It is this routine that is timed.
-		* It is called only on the parent.
-		*/
+void gauss();
 
 /* returns a seed for srand based on the time */
 unsigned int time_seed() {
@@ -36,11 +34,8 @@ unsigned int time_seed() {
 
 /* Set the program parameters from the command-line arguments */
 void parameters(int argc, char **argv) {
-	int seed = 0;  /* Random seed */
-	// char uid[32]; /*User name */
-
-	/* Read command-line arguments */
-	srand(time_seed());  /* Randomize */
+	int seed = 0; 
+	srand(time_seed());
 
 	if (argc == 4) {
 		seed = atoi(argv[2]);
@@ -111,28 +106,34 @@ void print_X() {
 
 int main(int argc, char **argv) {
   /* Timing variables */
-  struct timeval etstart, etstop;  /* Elapsed times using gettimeofday() */
+  struct timeval etstart, etstop;
   struct timezone tzdummy;
-  // clock_t etstart2, etstop2;  /* Elapsed times using times() */
   unsigned long long usecstart, usecstop;
-  struct tms cputstart, cputstop;  /* CPU times for my processes */
+  struct tms cputstart, cputstop;
 
-  /* Process program parameters */
   parameters(argc, argv);
-
-  /* Initialize A and B */
   initialize_inputs();
-
-  /* Print input matrices */
   print_inputs();
 
-  /* Start Clock */
   printf("\nStarting clock.\n");
   gettimeofday(&etstart, &tzdummy);
   times(&cputstart);
 
-  /* Gaussian Elimination */
-  gauss();
+  pthread_t threads[numThreads];
+  struct thread_args* args = malloc(numThreads * sizeOf(struct thread_args));
+
+  int norm, index;
+  for (int index = 0; index < numThreads; index++) {
+    for (norm = index; norm < N - 1; norm+=numThreads) {
+        args[index].id = index;
+        args[index].norm = norm;
+        pthread_create(&threads[index], NULL, gauss, (void *) &args[index]); 
+    }
+  }
+
+  for (index = 0; index < numThreads; index++) {
+    pthread_join(threads[index], NULL);
+  }
 
   /* Stop Clock */
   gettimeofday(&etstop, &tzdummy);
@@ -141,10 +142,8 @@ int main(int argc, char **argv) {
   usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
   usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
 
-  /* Display output */
   print_X();
 
-  /* Display timing results */
   printf("\nElapsed time = %g ms.\n",
 	 (float)(usecstop - usecstart)/(float)1000);
 
@@ -161,41 +160,36 @@ int main(int argc, char **argv) {
 	 (float)( (cputstop.tms_cutime + cputstop.tms_cstime) -
 		  (cputstart.tms_cutime + cputstart.tms_cstime) ) /
 	 (float)CLOCKS_PER_SEC * 1000);
-      /* Contrary to the man pages, this appears not to include the parent */
   printf("--------------------------------------------\n");
   
   exit(0);
 }
 
 /* ------------------ Above Was Provided --------------------- */
-void gauss() {
-  int norm, row, col;  /* Normalization row, and zeroing
-			* element row and col */
+void *gauss(void * args) {
+  int norm, row, col;
   float multiplier;
 
-  printf("Computing Serially.\n");
+  struct thread_args* thread_a = (struct thread_args*) args;
+  int id = thread_a->id;
+  norm = thread_a->norm;
 
   /* Gaussian elimination */
-  for (norm = 0; norm < N - 1; norm++) {
     for (row = norm + 1; row < N; row++) {
-      multiplier = A[row][norm] / A[norm][norm];
-      for (col = norm; col < N; col++) {
-	      A[row][col] -= A[norm][col] * multiplier;
-      }
-      B[row] -= B[norm] * multiplier;
+        multiplier = A[row][norm] / A[norm][norm];
+        for (col = norm; col < N; col++) {
+            A[row][col] -= A[norm][col] * multiplier;
+        }
+        B[row] -= B[norm] * multiplier;
     }
-  }
-  /* (Diagonal elements are not normalized to 1.  This is treated in back
-   * substitution.)
-   */
 
-
-  /* Back substitution */
-  for (row = N - 1; row >= 0; row--) {
-    X[row] = B[row];
-    for (col = N-1; col > row; col--) {
-      X[row] -= A[row][col] * X[col];
+  if (id == 0) {
+    for (row = N - 1; row >= 0; row--) {
+        X[row] = B[row];
+        for (col = N-1; col > row; col--) {
+        X[row] -= A[row][col] * X[col];
+        }
+        X[row] /= A[row][row];
     }
-    X[row] /= A[row][row];
   }
 }
